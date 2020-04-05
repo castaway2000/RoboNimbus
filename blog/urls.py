db3e1aea6ac7a3da57b12e96aa7d0004801a788e
Adam Szablya <aszablya@tourzan.com>
2020-04-04 18:14:03 -0700

    update blog urls to be more seo friendly

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.BlogPostsListView.as_view(), name='blog'),
-    path('blog/<slug>/', views.BlogPostView.as_view(), name='blog_post'),
-    path('blog-category/<slug>/', views.BlogCategoryView.as_view(), name='blog_category'),
+    path('blog', views.BlogPostsListView.as_view(), name='blog'),
+    path('blog/<slug>', views.BlogPostView.as_view(), name='blog_post'),
+    path('blog-category/<slug>', views.BlogCategoryView.as_view(), name='blog_category'),
 ]
